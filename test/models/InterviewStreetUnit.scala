f36a0bf8624fa7c1e75564e6b18f16a79d1f081f
Nelson Chen <crazysim@gmail.com>
2012-10-19 23:14:32 -0700

    contains

--- test/models/InterviewStreetUnit.scala
+++ test/models/InterviewStreetUnit.scala
@@ -1,16 +1,21 @@
 package models
 
 import org.specs2.mutable._
+import org.specs2.matcher.StringMatchers.contain
 import play.api.test._
 import play.api.test.Helpers._
 
 import InterviewStreet._
 import scala.io.Source
 
-class InterviewStreetTest extends Specification {
+class InterviewStreetUnit extends Specification {
   "InterviewStreet" should {
     "be tested with a resource loader that works" in {
-
+      val json = getJSON("leader_hk.json")
+      json must contain("models")
+    }
+    "test right" in {
+      true
     }
   }
 
