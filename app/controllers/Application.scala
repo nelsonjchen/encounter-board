1fe7b587141dbe8fa42ee0469e601b9fa3561af0
Nelson Chen <crazysim@gmail.com>
2012-10-20 01:39:01 -0700

    added hack dash

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,23 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import libs.concurrent.Akka
+import models.InterviewStreet
+import play.api.Play.current
 
 object Application extends Controller {
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def hack_dash = Action {
+    val promiseOfTeams = Akka.future {
+      InterviewStreet.test_scrape()
+    }
+    Async{
+      promiseOfTeams.map(lt => Ok(lt.toString()))
+    }
   }
   
 }
