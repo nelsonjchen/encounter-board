1990af978208bfb7cc9548f5f59a8881db8eaef0
Nelson Chen <crazysim@gmail.com>
2012-10-20 00:00:25 -0700

    parsers

--- app/models/InterviewStreet.scala
+++ app/models/InterviewStreet.scala
@@ -1,11 +1,16 @@
 package models
 
+import play.api.libs.json.Json
+
 case class Team(name: String, rank: Int, complete: List[String]) extends Ordered[Team] {
   def compare(that: Team) = rank.compare(that.rank)
 }
 
 object InterviewStreet {
   def parseTeam(leader_json: String, solved_json: String): Team = {
+    val leader = Json.parse(leader_json)
+    val solved = Json.parse(solved_json)
+
     Team("TestTeam", 45, List[String]())
   }
 
