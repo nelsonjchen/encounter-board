4d3c512f8f7ccf721a1c8e9e54756962e8178853
Nelson Chen <crazysim@gmail.com>
2012-10-19 22:47:01 -0700

    rough signatures

--- app/models/InterviewStreet.scala
+++ app/models/InterviewStreet.scala
@@ -1,5 +1,13 @@
 package models
 
-object InterviewStreet {
+case class Team(name: String, rank: Int, complete: List[String])
 
+object InterviewStreet {
+  def parseTeam(leader_json: String, solved_json: String): Team = {
+    Team("TestTeam", 45, List[String]())
+  }
+
+  def scores():List[Team] = {
+    List[Team]()
+  }
 }
