ed128f48c5facc267446fa259245a089871d71ae
Nelson Chen <crazysim@gmail.com>
2012-10-19 22:19:57 -0700

    add more dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,9 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "org.apache.httpcomponents" % "httpclient" % "4.2.1",
+      "net.sourceforge.htmlunit" % "htmlunit" % "2.10",
+      "org.ccil.cowan.tagsoup" % "tagsoup" % "1.2.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
