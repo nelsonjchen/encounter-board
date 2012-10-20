911e1ef4e024970106fe5a33aaae888ca51ef24f
Nelson Chen <crazysim@gmail.com>
2012-10-19 22:30:24 -0700

    rough test ready to go

--- test/models/InterviewStreetTest.scala
+++ test/models/InterviewStreetTest.scala
@@ -1,5 +1,14 @@
 package models
 
-class InterviewStreetTest {
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+
+class InterviewStreetTest extends Specification {
+  "InterviewStreet" should {
+    "parse" in {
+      1 mustEqual 1
+    }
+  }
 
 }
